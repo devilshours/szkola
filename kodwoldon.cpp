4930e80418e50bff26cb9a2bd8253411ff11fb0b
devilshours <plasmacavegg@gmail.com>
2024-01-17 00:43:21 +0100

    do edycji

--- kodwoldon.cpp
+++ kodwoldon.cpp
@@ -3,26 +3,47 @@
 #include <ctime>
 using namespace std;
 
-void wypelnijTab() { 
-    int a[12];
-
+void wypelnijTab(int a[], int size) { 
     srand( time( NULL ) );
-    for(int i=0; i<12; i++)
+    for(int i = 0; i < size; i++)
     {
-        a[i]=rand()%100+1;
-    }
-    for (int i=0; i<12; i++)
-    {
-        cout << a[i] << " ";
+        a[i] = rand() % 100 + 1;
     }
 }
 
+void sortujBombelkowo(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // zamiana miejscami
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 
+void drukujTab(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 
 int main(){
+    const int rozmiar = 12;
+    int a[rozmiar];
     
-wypelnijTab();
+    wypelnijTab(a, rozmiar);
+    
+    cout << "Tablica przed sortowaniem bombelkowym:\n";
+    drukujTab(a, rozmiar);
 
+    sortujBombelkowo(a, rozmiar);
+
+    cout << "\n\nTablica po sortowaniu bombelkowym:\n";
+    drukujTab(a, rozmiar);
 
     return 0;
 }
