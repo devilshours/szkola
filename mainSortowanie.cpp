f447db0c7eb0092312967dbe7ee0ac42deaa9956
dev$on <66412889+devilshours@users.noreply.github.com>
2024-02-14 11:18:42 +0100

    Update mainSortowanie.cpp

--- mainSortowanie.cpp
+++ mainSortowanie.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include "AlgorytmySortowania.hpp"
 
+using namespace std;
+
+int main(){
+
+
+
+    wypelnijTab();
+    drukujTab();
+    cout << "<-- nieposortowana tablica\n";
+    SortujBombelkowo(tab, n);
+    drukujTab();
+    cout <<  "<-- posortowana tablica\n";
+
+    wypelnijTab();
+    drukujTab();
+    cout << "<-- nieposortowana tablica\n";
+    SortujKoktajlowo(tab, n);
+    drukujTab();
+    cout <<  "<-- posortowana tablica\n";
+
+    wypelnijTab();
+    drukujTab();
+    cout << "<-- nieposortowana tablica\n";
+    SortujScalaniem(tab, 0, n - 1);
+    drukujTab();
+    cout <<  "<-- posortowana tablica\n";
+
+    wypelnijTab();
+    drukujTab();
+    cout << "<-- nieposortowana tablica\n";
+    SortujSzybko(tab, 0, n - 1);
+    drukujTab();
+    cout <<  "<-- posortowana tablica\n";
+
+    wypelnijTab();
+    drukujTab();
+    cout << "<-- nieposortowana tablica\n";
+    SortujPrzezWstawianie(tab, n);
+    drukujTab();
+    cout <<  "<-- posortowana tablica\n";
+
+    return 0;
+}
