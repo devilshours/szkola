1149db90d8b6272793aafb3c14f0a13a6e473fbf
dev$on <66412889+devilshours@users.noreply.github.com>
2024-02-14 15:39:30 +0100

    Update AlgorytmWyszukiwania.cpp

--- AlgorytmWyszukiwania.cpp
+++ AlgorytmWyszukiwania.cpp
@@ -72,7 +72,9 @@ int binarne(int tab[], int size, int key) {
 int main() {
     int tab[] = {1, 3, 5, 7, 9, 11, 13, 15};
     int size = sizeof(tab) / sizeof(tab[0]);
-    int key = 9;
+    int key;
+    cout << "Podaj indeks klucza: ";
+    cin >> key;
 
     cout << "Wyszukiwanie liniowe: " << liniowe(tab, size, key) << endl;
     cout << "Wyszukiwanie liniowe z wartownikiem: " << liniowezWartownikiem(tab, size, key) << endl;
