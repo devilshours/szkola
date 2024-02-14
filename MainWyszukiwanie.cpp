4fbd13298fa536aa6d8bf6786c6842527165a81a
dev$on <66412889+devilshours@users.noreply.github.com>
2024-02-14 11:25:29 +0100

    Update MainWyszukiwanie.cpp

--- MainWyszukiwanie.cpp
+++ MainWyszukiwanie.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <cmath>
 
+int main() {
+    int tab[] = {1, 3, 5, 7, 9, 11, 13, 15};
+    int size = sizeof(tab) / sizeof(tab[0]);
+    int key = 9;
+
+    cout << "Wyszukiwanie liniowe: " << liniowe(tab, size, key) << endl;
+    cout << "Wyszukiwanie liniowe z wartownikiem: " << liniowezWartownikiem(tab, size, key) << endl;
+    cout << "Wyszukiwanie skokowe: " << skokowe(tab, size, key) << endl;
+    cout << "Wyszukiwanie binarne: " << binarne(tab, size, key) << endl;
+
+    return 0;
+}
