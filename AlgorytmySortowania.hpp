67411773f1239ba6f4df0576d37464801a787f16
dev$on <66412889+devilshours@users.noreply.github.com>
2024-02-14 11:22:48 +0100

    Update AlgorytmySortowania.hpp

--- AlgorytmySortowania.hpp
+++ AlgorytmySortowania.hpp
@@ -3,6 +3,7 @@ void SortujBombelkowo(int tab[], int n);
 void SortujKoktajlowo(int tab[], int rozmiar);
 
 void Scal(int tab[], int lewy, int srodek, int prawy);
+int* SortujScalaniem(int arr[], int l, int r);
 
 
 
